c60799d887db8a2959a8d37eb5351100a37dd0ba
Ihor Yarovyy <Ihor.Yarovyy@ringcentral.com>
2023-07-26 15:45:29 +0300

    Show result of recognition and audio player

--- si-client/src/App.tsx
+++ si-client/src/App.tsx
@@ -7,38 +7,47 @@ import SoundCheck from '@pages/SoundCheck';
 import ProfileMenu from '@components/ProfileMenu'
 import { AuthProvider } from '@contexts/index';
 import { PrivateRoute } from "@components/PrivateRoute"
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  // palette: {
+  //   mode: "dark",
+  // },
+});
 
 function App() {
 
   return (
-    <AuthProvider>
-      <div>
-        <AppBar position="static">
-          <Toolbar variant="dense">
-            <Box display="flex" justifyContent="flex-end" width="100%">
-              <ProfileMenu />
-            </Box>
-          </Toolbar>
-        </AppBar>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <div>
+          <AppBar position="static">
+            <Toolbar variant="dense">
+              <Box display="flex" justifyContent="flex-end" width="100%">
+                <ProfileMenu />
+              </Box>
+            </Toolbar>
+          </AppBar>
 
-        <Box width={500} m="auto" mt={2}>
-          <Switch>
-            {/* <Route path={paths.main} exact>
-              <Auth/>
-            </Route> */}
-            <PrivateRoute path={paths.main} exact>
-              <SoundCheck />
-            </PrivateRoute>
-            <PrivateRoute path={paths.soundcheck} exact>
-              <SoundCheck />
-            </PrivateRoute>
-            <Route path={paths.auth} exact>
-              <Auth />
-            </Route>
-          </Switch>
-        </Box>
-      </div>
-    </ AuthProvider>
+          <Box width={500} m="auto" mt={2}>
+            <Switch>
+              {/* <Route path={paths.main} exact>
+                <Auth/>
+              </Route> */}
+              <PrivateRoute path={paths.main} exact>
+                <SoundCheck />
+              </PrivateRoute>
+              <PrivateRoute path={paths.soundcheck} exact>
+                <SoundCheck />
+              </PrivateRoute>
+              <Route path={paths.auth} exact>
+                <Auth />
+              </Route>
+            </Switch>
+          </Box>
+        </div>
+      </ AuthProvider>
+    </ThemeProvider>
   );
 }
 
--- si-client/src/pages/SoundCheck.tsx
+++ si-client/src/pages/SoundCheck.tsx
@@ -9,6 +9,9 @@ import { BASE_PATH } from '@shared/api/axios-client/base';
 
 const SoundCheck: FC = () => {
     const [file, setFile] = useState<File | null>(null)
+    const [recognizedSpeaker, setRecognizedSpeaker] = useState<String | null>(null)
+    // const [audio, setAudio] = useAudio<HTMLAudioElement | null>(null)
+
 
 
     const onSubmit = async (event: React.SyntheticEvent) => {
@@ -22,9 +25,14 @@ const SoundCheck: FC = () => {
             data: formData
         }
         const resp = await siApi.predictPost(options)
-        alert(JSON.stringify(resp.data));
+        setRecognizedSpeaker(resp.data.predictions[0].prediction[0])
     };
 
+    const handleChange = (value) => {
+        setRecognizedSpeaker(null)
+        setFile(value)
+        // setAudio(URL.createObjectURL(value))
+    }
 
     return (
         <Box
@@ -49,15 +57,33 @@ const SoundCheck: FC = () => {
                     <MuiFileInput
                         value={file}
                         placeholder="Select a file"
-                        onChange={setFile}
+                        onChange={handleChange}
                     />
                 </Box>
+                {file && (
+                <Box mb={2}>
+                     <audio
+                        controls 
+                        nodownload
+                        nofullscreen
+                        src={URL.createObjectURL(file)}
+                        style={{ width: '100%' }}>
+                    </audio>
+                </Box>
+                )}
+                {recognizedSpeaker &&(
+                <Box mb={2} bgcolor="#91e3bd">
+                    <Typography display="block" variant="h5" component="h5" align='center'>
+                    Voice of speracker: {recognizedSpeaker}
+                </Typography>
+                </Box>
+                ) }
                 <Button
                     type="submit"
                     variant="contained"
                     color="primary"
                     size="large"
-                    disabled={!file}
+                    disabled={!file || recognizedSpeaker}
                     fullWidth
                 >
                     Recognize
